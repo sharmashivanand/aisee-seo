7492c350704836536e8c2c0b78f390c36e5b01a9
www-data <shivanand@binaryturf.com>
2019-04-14 11:52:41 +0000

    test

--- aisee.php
+++ aisee.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: AISee SEO
- * Plugin URI:  https://aiseeseo.com
- * Description: GSC Insights for content optimizers. Fetch SEO clicks, impressions, CTR and SERPS position of posts and pages from Google Search Console (Google Webmaster Tools)
+ * Plugin URI:  https://converticacommerce.com
+ * Description: Keyword visualization, tag cloud generator and LSI keyword helper.
  * Version:     1.0.0
  * Author:      Shivanand Sharma
  * Author URI:  https://www.converticacommerce.com
@@ -12,7 +12,7 @@
  */
 
 define( 'AISEEFILE', __FILE__ );
-define( 'AISEEAPIEP', 'https://aiseeseo.com/?p=9' );
+
 
 class AISee {
     function __construct(){
@@ -38,104 +38,184 @@ class AISee {
     }
 
     function hooks(){
-        add_action( 'admin_menu', array( $this, 'settings_menu' ) );
-        add_action( 'admin_head', array( $this, 'admin_style' ) );
-        add_action( 'admin_init', array($this, 'register_settings' ) );
-
+        add_action( 'add_meta_boxes', array( $this,'add_meta_boxes' ) ); // add metaboxes
+        add_action( 'admin_enqueue_scripts', array( $this, 'plugin_styles' ) ); // enqueue plugin styles but only on the specific screen
+        add_action( 'wp_ajax_aisee_tag_cloud', array( $this, 'aisee_tag_cloud' )); // respond to ajax
+        add_action( 'wp_ajax_nopriv_aisee_tag_cloud', '__return_false' ); // do not respont to ajax
     }
 
-    function settings_menu(){
-        $hook_suffix = add_menu_page(
-			'AISee SEO', 
-			'AISee SEO', 
-			'manage_options', 
-			'aiseeseo', 
-            array( $this, 'settings_page' ),
-            $this->uri . 'assets/brand-icon.svg'
-        );
-        add_submenu_page( 'wpmr', 'Run Scan', 'Run Scan', 'manage_options' , 'wpmr', array($this, 'settings_page'));
-        //add_submenu_page( 'wpmr', 'Tools', 'Tools', 'manage_options' , 'wpmr-tools', array($this, 'wpmrtools'));
+    function plugin_styles(){
+        $screen = get_current_screen();
+		if( in_array( $screen->post_type , get_post_types(array( 'public' => true) ) ) ) {
+			wp_enqueue_style( 'aisee-stylesheet', $this->uri . 'assets/admin-styles.css' );
+		}
     }
 
-    function defaults() {
-        $defaults = array(
-            'connection' => '',
-        );
-        return $defaults;
+    function add_meta_boxes(){
+        foreach (get_post_types(array( 'public' => true)) as $post_type) {
+            add_meta_box( 'aisee-tag', __( 'AiSee Tag Cloud', 'aisee' ), array($this, 'aisee_tag_cloud_mb'), $post_type, 'normal');
+        }
     }
 
-    function register_settings(){
-        register_setting( 'aiseeseo_group', 'aiseeseo', array( $this, 'sanitize' ));
-        add_settings_section( 'aiseeseo_main', __( 'Main', 'aisee' ), array( $this,'main_section_text' ), 'aiseeseo' );
-        add_settings_field( 'aiseeseo_connection', __('Connect', 'aisee' ), array( $this, 'connect' ), 'aiseeseo', 'aiseeseo_main' );
+    function stop_words(){
+        return apply_filters('aisee_stop_words', array('I','I\'d','I\'ll','I\'m','I\'ve','a','about','above','after','again','against','all','am','an','and','any','are','aren\'t','as','at','be','because','been','before','being','below','between','both','but','by','can','can\'t','cannot','com','could','couldn\'t','did','didn\'t','do','does','doesn\'t','doing','don\'t','down','during','each','few','for','from','further','had','hadn\'t','has','hasn\'t','have','haven\'t','having','he','he\'d','he\'ll','he\'s','her','here','here\'s','hers','herself','him','himself','his','how','how\'s','i','i\'d','i\'ll','i\'m','i\'ve','if','in','into','is','isn\'t','it','it\'s','its','itself','let\'s','me','more','most','mustn\'t','my','myself','net','no','nor','not','of','off','on','once','only','or','org','other','ought','our','ours','ourselves','out','over','own','same','shan\'t','she','she\'d','she\'ll','she\'s','should','shouldn\'t','so','some','such','than','that','that\'s','the','their','theirs','them','themselves','then','there','there\'s','these','they','they\'d','they\'ll','they\'re','they\'ve','this','those','through','to','too','use','add','jan','feb','mar','apr','jun','jul','aug','sep','oct','nov','dec','under','until','up','very','was','wasn\'t','we','we\'d','we\'ll','we\'re','we\'ve','were','weren\'t','what','what\'s','when','when\'s','where','where\'s','which','while','who','who\'s','whom','why','why\'s','with','will','won\'t','would','wouldn\'t','www','you','you\'d','you\'ll','you\'re','you\'ve','your','yours','yourself','yourselves','http','https','io','get'));
     }
 
-    function main_section_text(){
-        echo '<p>Please provide the following.</p>';
-    }
+    function aisee_tag_cloud(){
 
-    function connect(){
-        $statevars = array(
-            'origin_site_url' => get_site_url(),
-            'origin_nonce' => wp_create_nonce( 'wprtsp_gaapi' ),
-            'origin_ajaxurl' => admin_url( 'admin-ajax.php' ),
-            'return_url' => admin_url('admin.php?page=aiseeseo')
-        );
-        $statevars = $this->encode($statevars);
-        $auth = esc_url( add_query_arg( 'g_authenticate', $statevars, AISEEAPIEP ) );
-        $revoke = esc_url( add_query_arg( 'g_revoke', $statevars, AISEEAPIEP ) );
-        //$this->llog($statevars);
-        //$this->llog($this->decode($statevars));
+        check_ajax_referer( 'aisee_tag_cloud', 'aisee_tag_cloud_nonce' );
+        
+        // Don't get caught pants down... again
+        $post_id         = !empty($_REQUEST['post_id']) ? sanitize_text_field($_REQUEST['post_id']) : 0 ;
+        $trimlen         = !empty($_REQUEST['trim']) ?  (int) sanitize_text_field($_REQUEST['trim']) : 0;
+        $drop_percentage = !empty($_REQUEST['drop_percentage'])? sanitize_text_field($_REQUEST['drop_percentage']) : 0;
+                    
+        $status = get_post_status($post_id);
+        
+        if(! $status ) {
+            wp_send_json_error( 'Post does not exist.' );
+        }
 
-        ?>
-        <input type="hidden" <?php echo $readonly ?> id="connect" name="aiseeseo['aiseeseo_connection']" value="<?php echo esc_attr( $this->get_setting('aiseeseo_connection')); ?>" />
-        <?php
-        if( ! $ga_profile) { 
-            ?>
-            <a class="button-primary" href="<?php echo $auth ?>">Connect with Google Services</a>
-            <?php
+        $url = false;
+        if( $status == 'publish' ) {
+            $url = get_permalink($post_id);
         }
         else {
-            ?>
-            Profile Active: <?php echo $ga_profile; ?><br /><a href="<?php echo $revoke ?>" class="button-primary">Disconnect with Google Services</a>
-            <?php
+            wp_send_json_error( 'Please publish this post to see the tag cloud.' );
         }
+
+        $response = wp_safe_remote_request(
+            $url
+        );
+
+        if(is_wp_error($response)) {
+            wp_send_json_error( $response->get_error_message() );
+        }
+        
+        $status_code = wp_remote_retrieve_response_code( $response );
+
+        if( 200 != $status_code ) {
+            wp_send_json_error( 'Failed to fetch post content: ' . $status_code );
+        }
+        
+        $keywords = wp_remote_retrieve_body( $response );
+        
+        if(!$keywords) {
+            wp_send_json_error('Encountered empty content.' );
+        }
+        
+        $stop     = $this->stop_words();
+        $keywords = sanitize_text_field($keywords);
+
+        $dom = new DOMDocument();
+        $dom->loadHTML($keywords);
+
+        $_scripts=$dom->getElementsByTagName("script");
+        $scripts = array();
+        foreach ($_scripts as $script) {
+            $scripts[]=$script;
+        }
+
+        foreach ($scripts as $script) {
+            $script->parentNode->removeChild($script);
+        }
+
+        $_styles=$dom->getElementsByTagName("style");
+
+        $styles = array();
+        foreach ($_styles as $style) {
+            $styles[]=$style;
+        }
+
+        foreach ($styles as $style) {
+            $style->parentNode->removeChild($style);
+        }
+        
+        $keywords = $dom->saveHTML();
+        $pattern ='/(&[a-zA-Z]+);/';
+        
+        $keywords = preg_replace($pattern,' ', $keywords);
+        $keywords = urldecode($keywords);
+        $keywords = strtolower(strip_tags($keywords));
+        $keywords = preg_replace('/[^a-z0-9]/',' ', $keywords);
+        $keywords = preg_replace('/\s+/',' ', $keywords);
+        $keywords = explode(' ',$keywords);
+        $keywords = array_diff($keywords, $stop);
+        $tags     = array_count_values($keywords);
+
+        $tags = array_filter($tags, function($v,$k){
+            $k = preg_replace('/\b\d+\b/','',$k);
+            if( $v == 1 ) { // remove words that occur only once; they have no weight
+                return false;
+            }
+            if(strlen(trim($k)) <= $_REQUEST['trim'] ) {
+                return false;
+            }
+            return ! empty( trim( $k ) );
+        }, ARRAY_FILTER_USE_BOTH);
+        
+        arsort( $tags );
+
+        $newtags = array();
+        $avg     = ( max( $tags ) + min( $tags ) ) / 2;
+        $avg     = $avg / 1.618;                     // fine-tune the scaling here
+        
+        foreach($tags as $key => $value) {
+            if( ( $drop_percentage ) > ( ( $value * 100 ) / count( $tags ) )  ) {
+                continue;
+            }
+            $newtags[] = '<span class="aitag" style="font-size:'. ( 16.81 * ( ( $value + $avg ) / $avg ) ).'px">'.$key.'</span>';
+        }
+
+        for( $i=0; $i <= 49 ; $i++ ){
+            shuffle( $newtags );
+        }
+        arsort( $newtags );
+        $newtags = implode( ' ', $newtags );
+        
+        wp_send_json_success( $newtags );
     }
 
-    function encode($data){
-        $data = strtr( base64_encode( json_encode( $data ) ), '+/=', '-_,' );
-        return $data;
-    }
-
-    function decode($data){
-        return json_decode( base64_decode( strtr($data, '-_,', '+/=' ) ), true);
-    }
-
-    function get_setting( $setting ) {
-        $defaults = $this->defaults();
-        $settings = wp_parse_args( get_option( 'aiseeseo', $defaults ), $defaults );
-        return isset( $settings[ $setting ] ) ? $settings[ $setting ] : false;
-    }
-
-    function settings_page(){
+    function aisee_tag_cloud_mb(){
         ?>
-        <div class="wrap">
-        <form method="post" action="options.php">
-            <?php settings_fields( 'aiseeseo' ); ?>
-            <?php do_settings_sections( 'aiseeseo' ); ?>
-            <?php //submit_button(); ?>
-        </form>
-        </div>
+        <div id="aisee-tag-cloud"></div>
+        <p><label><strong>Drop words with density less than this percentage :</strong><br /><input type="number" id="aisee_drop_percentage" value="0.2" min="0" max="1" step=".1" /></label><br />Increase this to see a smaller tag cloud; decreasing results in a larger tag cloud</p>
+        <p><label><strong>Ignore words containing less than these many characters:</strong><br /><input type="number" id="aisee_trim_length" value="1" min="0" max="5" /></label></p>
         <?php
-    }
-
-    function sanitize( $settings ){
-        return $settings;
-    }
-
-    function admin_style(){ ?>
-        <style type="text/css">#toplevel_page_aiseeseo .wp-menu-image img { width: 20px; height: auto;opacity: 1;padding: 9px 0 0 0;}</style>
-        <?php
+        submit_button( 'Generate Tag Cloud', 'secondary', 'aisee-generate-tag-cloud');
+        ?>
+        <script type="text/javascript">
+        jQuery(document).ready(function ($) {
+            $('#aisee-generate-tag-cloud').click(function(e) {
+                e.preventDefault();
+                aisee_tag_cloud = {
+                    aisee_tag_cloud_nonce: '<?php echo wp_create_nonce( 'aisee_tag_cloud' ); ?>',
+                    action: "aisee_tag_cloud",
+                    drop_percentage : $('#aisee_drop_percentage').val(),
+                    trim : $('#aisee_trim_length').val(),
+                    cachebust: Date.now(),
+                    post_id : '<?php global $post; echo $post->ID; ?>',
+                };
+                $.ajax({
+                    url: ajaxurl,
+                    method: 'POST',
+                    data: aisee_tag_cloud,
+                    success: function (res) {
+                        $('#aisee-tag-cloud').html(res);
+                        console.dir(res);
+                        if(res.hasOwnProperty('success') && res.success == true && res.hasOwnProperty('data') && res.data.length) {
+                            $('#aisee-tag-cloud').html(res.data);
+                            console.dir(res.data);
+                        }
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        $('#aisee-tag-cloud').html(errorThrown);
+                    },
+                });
+            });
+        });
+    </script>
+    <?php
     }
 
     function llog($str){
--- readme.txt
+++ readme.txt
@@ -8,10 +8,16 @@ Stable tag: 1.0
 License: MIT
 License URI: https://opensource.org/licenses/MIT
 
-GSC Insights for content optimizers. Fetch SEO clicks, impressions, CTR and SERPS position of posts and pages from Google Search Console (Google Webmaster Tools)
+Generate keyword tag cloud from the entire post as it looks to the search bots and visitors.
 
 == Description ==
-GSC Insights for content optimizers. Fetch SEO clicks, impressions, CTR and SERPS position of posts and pages from Google Search Console (Google Webmaster Tools)
+Content optimization is key to search success. Do you ever wonder how your keyword landscape looks like? Clusters of jibberish crawled by search spiders, churned through so many algorithms… What are the prominent keywords in your content **mind you not just the post content**. Your theme, plugins, scripts, post title… there's a lot that comes into play while rendering the markup for search bots.
+
+The key is to get the search densitiy right so that the post content stays on topic rather than getting lost around tons and tons of meaning less words.
+
+### LSI Keywords:
+
+With AISee you can also visualize the LSI keywords. This feature is still evolving and improving.
 
 == Installation ==
 Upload the plugin to your blog. Activate it. Visit plugin settings Create a support thread in case of any issues.
@@ -21,8 +27,6 @@ Ask away.
 
 == Screenshots ==
 1. screenshot-1.png
-2. screenshot-2.png
-3. screenshot-3.png
 
 == Changelog ==
 
