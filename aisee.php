34d3d33d8e1951c2e6b9720047ed8766f874bc65
sharmashivanand <shivanand@binaryturf.com>
2019-04-18 08:51:41 +0000

    added cmb2

--- aisee.php
+++ aisee.php
@@ -46,6 +46,80 @@ class AISee {
         add_action( 'add_meta_boxes', array( $this,'add_meta_boxes' ) ); // add metaboxes
         add_action( 'admin_enqueue_scripts', array( $this, 'plugin_styles' ) ); // enqueue plugin styles but only on the specific screen
         add_action( 'admin_init', [$this, 'plugin_data']);
+        add_action( 'admin_head', array( $this, 'admin_style' ) );
+        add_action( 'admin_menu', array( $this, 'settings_menu' ) );
+
+    }
+
+    function settings_menu(){
+        add_menu_page('AISee', 'AISee', 'manage_options', 'aisee', array( $this, 'settings_page' ), $this->uri . 'assets/brand-icon.svg' );
+        add_submenu_page( 'aisee',  'AISee Setup' ,  'AISee Setup' , 'manage_options', 'aisee-setup', array( $this, 'admin_page' ) );
+    }
+
+    function settings_page() {
+        echo 'settings_page';
+    }
+    function admin_style(){ ?>
+        <style type="text/css">
+        .wp-has-submenu.toplevel_page_aisee .wp-menu-image img {
+            width: 34px;
+            height: auto;
+            padding: 4px !important;
+            animation: aihrot 10s ease-in-out infinite;
+            opacity: 1 !important;
+            box-sizing: border-box;
+            filter: saturate(2);
+        }
+
+        @keyframes aihrot {
+            0% {
+                filter: hue-rotate(0deg);
+            }
+            10% {
+                filter: hue-rotate(36deg);
+            }
+            20% {
+                filter: hue-rotate(72deg);
+            }
+            30% {
+                filter: hue-rotate(108deg);
+            }
+            40% {
+                filter: hue-rotate(144deg);
+            }
+            50% {
+                filter: hue-rotate(180deg);
+            }
+            60% {
+                filter: hue-rotate(216deg);
+            }
+            70% {
+                filter: hue-rotate(252deg);
+            }
+            80% {
+                filter: hue-rotate(288deg);
+            }
+            90% {
+                filter: hue-rotate(324deg);
+            }
+            100% {
+                filter: hue-rotate(360deg);
+            }
+        }
+        </style>
+        <?php
+    }
+    
+    function admin_page(){
+        if ( ob_get_length() ) {
+			ob_end_clean();
+        }
+        global $wp_version;
+        ob_start();
+        aisee_llog($this->plugin_data);
+        $out = ob_get_clean();
+        echo $out;
+        exit;
     }
 
     function plugin_data(){
